b01753cbb5e8d0540f3717aa775fa066d005a5cb
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2021-05-31 02:35:47 +0100

    Win -> Lin

--- static/util/fetch.js
+++ static/util/fetch.js
@@ -50,31 +50,3 @@ function postJSONmap(url,obj,map){
 function getJSONmap(url, map){
     return getJSON(url).then(elems => elems.map(map))
 }
-
-window.addEventListener("submit", async e => {
-    // e.target form submited
-    // e.submitter button pressed to submit
-    let form = e.target;
-    if( form.disabled ){
-        return;
-    }
-    let funName = form.dataset.submitCb;
-    if( !funName ){
-        return;
-    }
-    let fun = window[funName];
-    if( !fun ){
-        console.warn(`Form data-submit-cb is defined to ${funName}, however windows.${funName} is not defined.`);
-    }
-    form.disabled = true;
-    e.preventDefault();
-
-    const data = new FormData(form);
-    const value = Object.fromEntries(data.entries());
-    
-    requestJSON(form.getAttribute("action"), form.getAttribute("method"), value)
-        .then(value => {
-            form.disabled = false;
-            fun(value);
-        });
-})
